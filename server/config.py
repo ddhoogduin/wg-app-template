7725cd8402490895f9831ecfadf1ecbfcaf0b1b2
ddhoogduin <d.d.hoogduin@lumc.nl>
2019-07-08 14:02:18 +0200

    Changing structure to be submodules

--- server/config.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-from configs.Database import Database
-
-app = Flask(__name__)
-app = Database(app).get_app()
-db = SQLAlchemy(app)
-
--- testcases/keygenes/src/actions/client/clientManagerActions.js
+++
@@ -1,9 +0,0 @@
-import SascWebApi from '../../apis/SascWeb'
-import {clientManager} from '../../constants/types'
-
-
-export const unsetRedirect = () =>{
-    return{
-        type: clientManager.UNSET_REDIRECT
-    }
-}
